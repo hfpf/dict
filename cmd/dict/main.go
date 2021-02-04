e8829507293483f3c8e3195cb3572ea56fd28f63
皇甫 <hfpf@linhuiba.com>
2021-02-04 13:51:06 +0800

    Initial commit

---
+++ cmd/dict/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"flag"
+	"os"
+
+	pb "dict/api/helloworld/v1"
+	"dict/internal/service"
+	"github.com/go-kratos/kratos/v2"
+	grpcconf "github.com/go-kratos/kratos/v2/api/kratos/config/grpc"
+	httpconf "github.com/go-kratos/kratos/v2/api/kratos/config/http"
+	"github.com/go-kratos/kratos/v2/config"
+	"github.com/go-kratos/kratos/v2/config/source/file"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/log/stdlog"
+	"github.com/go-kratos/kratos/v2/transport/grpc"
+	"github.com/go-kratos/kratos/v2/transport/http"
+)
+
+// go build -ldflags "-X main.Version=x.y.z"
+var (
+	// Version is the version of the compiled software.
+	Version string
+	// Branch is current branch name the code is built off.
+	Branch string
+	// Revision is the short commit hash of source tree.
+	Revision string
+	// BuildDate is the date when the binary was built.
+	BuildDate string
+)
+
+var flagconf string
+
+func init() {
+	flag.StringVar(&flagconf, "conf", "../../configs", "config path, eg: -conf config.yaml")
+}
+
+func main() {
+	flag.Parse()
+	conf := config.New(config.WithSource(
+		file.NewSource(flagconf),
+	))
+	if err := conf.Load(); err != nil {
+		panic(err)
+	}
+
+	logger := stdlog.NewLogger(stdlog.Writer(os.Stdout))
+	defer logger.Close()
+
+	log := log.NewHelper("main", logger)
+	log.Infow(
+		"version", Version,
+		"branch", Branch,
+		"revision", Revision,
+		"build_date", BuildDate,
+	)
+
+	// build transport server
+	hc := new(httpconf.Server)
+	gc := new(grpcconf.Server)
+	if err := conf.Value("http.server").Scan(hc); err != nil {
+		panic(err)
+	}
+	if err := conf.Value("grpc.server").Scan(gc); err != nil {
+		panic(err)
+	}
+	httpSrv := http.NewServer(http.Apply(hc))
+	grpcSrv := grpc.NewServer(grpc.Apply(gc))
+
+	// register service
+	gs := service.NewGreeterService()
+	pb.RegisterGreeterServer(grpcSrv, gs)
+	pb.RegisterGreeterHTTPServer(httpSrv, gs)
+
+	// application lifecycle
+	app := kratos.New()
+	app.Append(httpSrv)
+	app.Append(grpcSrv)
+
+	// start and wait for stop signal
+	if err := app.Run(); err != nil {
+		log.Errorf("start failed: %v\n", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module dict
+
+go 1.15
+
+require (
+	github.com/go-kratos/kratos/v2 v2.0.0-20210201151837-244c98e529c3
+	github.com/golang/protobuf v1.4.3
+	google.golang.org/genproto v0.0.0-20210114201628-6edceaf6022f
+	google.golang.org/grpc v1.35.0
+	google.golang.org/protobuf v1.25.0
+)
